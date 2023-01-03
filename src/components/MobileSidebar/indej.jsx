e71842072a8821fd05dd44140115d2bf9554b725
oluwakemi <oluwakemi@semomitor.com>
2023-01-03 01:06:46 +0100

    sidebar functionalities

--- src/components/MobileSidebar/indej.jsx
+++ src/components/MobileSidebar/indej.jsx
@@ -1,8 +1,13 @@
+import './styles.scss'
 const MobileSidebar = ({onClose, show}) => {
     return (
-        <div className="mobile-sidebarwrapper">
+        <div className={`mobile-sidebarwrapper ${show === true ? 'active': ''}`}>
+            <div className="sidebar">
+             <button onClick={onClose}>dhd</button>
+            </div>
             
 
         </div>
     )
 }
+export default MobileSidebar
--- src/routes/Layout/index.jsx
+++ src/routes/Layout/index.jsx
@@ -1,4 +1,4 @@
-import {  useContext } from "react";
+import { Fragment, useContext , useState} from "react";
 import { Link, Outlet } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.content";
 
@@ -12,69 +12,79 @@ import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import "./layout.scss";
+import MobileSidebar from "../../components/MobileSidebar/indej";
+
 
 const Layout = () => {
   const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
+
   const { theme, setTheme } = useContext(ThemeContext);
+
+  const [ showSidebar, setShowSidebar ] = useState(false);
 
   const handleThemeChange = () => {
     const isCurrentDark = theme === "dark";
     setTheme(isCurrentDark ? "light" : "dark");
     localStorage.setItem("default-theme", isCurrentDark ? "light" : "dark");
   };
+  const toggleSidebar = () => setShowSidebar((prevState) => !prevState);
 
   const handleSignOut = async () => {
     await signOutUser();
   };
   return (
-    <div className="wrapper">
-      <div className="main-menu">
-        <Link className="logo-container" to="/">
-          <CrwnLogo className="logo" />
-        </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="shop">
-            SHOP
+    <Fragment>
+      <div className="wrapper">
+        <div className="main-menu">
+          <Link className="logo-container" to="/">
+            <CrwnLogo className="logo" />
           </Link>
-          {currentUser ? (
-            <span className="nav-link" onClick={handleSignOut}>
-              SIGN OUT
-            </span>
-          ) : (
-            <Link className="nav-link" to="auth">
-              SIGN-IN
+          <div className="nav-links-container">
+            <Link className="nav-link" to="shop">
+              SHOP
             </Link>
-          )}
-          <CartIcon />
-        </div>
-      </div>
-      <div className="content">
-        <div className="navbar">
-          <div className="navigation">{isCartOpen && <CartDropdown />}</div>
-          <div className="toggle-btn-section">
-            <div className={`toggle-checkbox m-vertical-auto`}>
-              <input
-                className="toggle-btn__input"
-                type="checkbox"
-                name="checkbox"
-                onChange={handleThemeChange}
-                checked={theme === "light"}
-              />
-              <button
-                type="button"
-                className={`toggle-btn__input-label`}
-                onClick={handleThemeChange}
-              ></button>
-            </div>
+            {currentUser ? (
+              <span className="nav-link" onClick={handleSignOut}>
+                SIGN OUT
+              </span>
+            ) : (
+              <Link className="nav-link" to="auth">
+                SIGN-IN
+              </Link>
+            )}
+            <CartIcon />
           </div>
         </div>
-        <div className="header-navbar-shadow"></div>
-        <main className="routes-wrapper">
-          <Outlet />
-        </main>
+        <div className="content">
+          <div className="navbar">
+            <div className="navigation">{isCartOpen && <CartDropdown />}</div>
+            <button onClick={toggleSidebar}>d</button>
+            <div className="toggle-btn-section">
+              <div className={`toggle-checkbox m-vertical-auto`}>
+                <input
+                  className="toggle-btn__input"
+                  type="checkbox"
+                  name="checkbox"
+                  onChange={handleThemeChange}
+                  checked={theme === "light"}
+                />
+                <button
+                  type="button"
+                  className={`toggle-btn__input-label`}
+                  onClick={handleThemeChange}
+                ></button>
+              </div>
+            </div>
+          </div>
+          <div className="header-navbar-shadow"></div>
+          <main className="routes-wrapper">
+            <Outlet />
+          </main>
+        </div>
       </div>
-    </div>
+      <MobileSidebar show={showSidebar} onClose={toggleSidebar} />
+    </Fragment>
   );
 };
 
