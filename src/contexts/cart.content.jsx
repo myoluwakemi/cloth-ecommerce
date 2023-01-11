b1e22d1562a05d19d5c3a7fe6a060ffbf2492327
oluwakemi <yusufoluwakemi39@gmail.com>
2023-01-11 17:54:32 +0100

    worked on the checkout page and added functionalities

--- src/contexts/cart.content.jsx
+++ src/contexts/cart.content.jsx
@@ -1,11 +1,12 @@
 import { createContext, useState, useEffect } from "react";
- 
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
-  cartCount: 0
+  removeItemFromCart: () => {},
+  cartCount: 0,
 });
 
 const addCartItem = (cartItems, productToAdd) => {
@@ -25,25 +26,53 @@ const addCartItem = (cartItems, productToAdd) => {
   // return new array with modified cartitems/ new cart item
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  //find the cart item to remove
+  const existingCartItems = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  //check if quantity is equal to 1, if it is remove from the cart
+  if (existingCartItems.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  // return back cartitem with matching cart with reduced quantity
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
-  const [cartCount, setCartCount] = useState(0)
-
+  const [cartCount, setCartCount] = useState(0);
 
   //reduce takes two arguement, 1: callback 2: starting value
-  useEffect(()=> {
-    const newCartCount = cartItems.reduce((total, cartItem)=> total + cartItem.quantity, 0)
-    setCartCount(newCartCount)
-
-  }, [cartItems])
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
+  const removeItemFromCart = (cartItemToRemove) => {
+    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
 
-
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount };
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    removeItemFromCart,
+    cartItems,
+    cartCount,
+  };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,8 +1,111 @@
-import './checkout.styles.scss'
+import { useContext } from "react";
+import classnames from "classnames";
+import { CartContext } from "../../contexts/cart.content";
+import { Card, CardBody, CardText, Button, Badge } from "reactstrap";
+import { Star, X, Plus, Minus, Heart } from "react-feather";
+import "./checkout.styles.scss";
+import { Link } from "react-router-dom";
 const CheckOut = () => {
-    return(
-        <div></div>
+  const { cartItems , addItemToCart,removeItemFromCart} = useContext(CartContext);
 
-    )
-}
-export default CheckOut
+  const addItemToCartHandler = (item) => {
+    addItemToCart()
+  }
+  return (
+    <div>
+      THis is checkout
+      <div className="list-view product-checkout">
+        {cartItems.map((item) => {
+          const { id, name, quantity, imageUrl } = item;
+          return (
+            <Card key={id} className="ecommerce-card">
+              <div className="item-img">
+                <Link to={`/apps/ecommerce/product-detail/${item.slug}`}>
+                  <img className="img-fluid" src={imageUrl} alt={name} />
+                </Link>
+              </div>
+              <CardBody>
+                <div className="item-name">
+                  <h6 className="mb-0">
+                    <Link to={`/apps/ecommerce/product-detail/${item.slug}`}>
+                      {name}
+                    </Link>
+                  </h6>
+                  <span className="item-company">
+                    By
+                    <a
+                      className="ms-25"
+                      href="/"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      {item.brand}
+                    </a>
+                  </span>
+                  <div className="item-rating">
+                    <ul className="unstyled-list list-inline">
+                      {new Array(5).fill().map((listItem, index) => {
+                        return (
+                          <li key={index} className="ratings-list-item me-25">
+                            <Star
+                              className={classnames({
+                                "filled-star": index + 1 <= item.rating,
+                                "unfilled-star": index + 1 > item.rating,
+                              })}
+                            />
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                </div>
+                <span className="text-success mb-1">In Stock</span>
+                <div className="item-quantity">
+                  <span className="quantity-title me-50">Qty</span>
+                  <div onClick={() => addItemToCart(item)} className="arrow">
+                    &#10094;
+                  </div>
+                  <span className="value">{quantity}</span>
+                  <div onClick={()=> removeItemFromCart(item)} className="arrow">
+                    &#10095;
+                  </div>
+                </div>
+                {/* <div className='delivery-date text-muted'>Delivery by, {formatDate(item.shippingDate)}</div> */}
+                <span className="text-success">
+                  {item.discountPercentage}% off {item.offers} offers Available
+                </span>
+              </CardBody>
+              <div className="item-options text-center">
+                <div className="item-wrapper">
+                  <div className="item-cost">
+                    <h4 className="item-price">${item.price}</h4>
+                    {item.hasFreeShipping ? (
+                      <CardText className="shipping">
+                        <Badge color="light-success" pill>
+                          Free Shipping
+                        </Badge>
+                      </CardText>
+                    ) : null}
+                  </div>
+                </div>
+                <Button className="mt-1 remove-wishlist" color="light">
+                  <X size={14} className="me-25" />
+                  <span>Remove</span>
+                </Button>
+                <Button className="btn-cart" color="primary">
+                  <Heart
+                    size={14}
+                    className={classnames("me-25", {
+                      "fill-current": item.isInWishlist,
+                    })}
+                  />
+                  <span className="text-truncate">Wishlist</span>
+                </Button>
+              </div>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default CheckOut;
